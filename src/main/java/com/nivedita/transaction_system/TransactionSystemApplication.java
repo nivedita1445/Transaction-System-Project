66cab03b1bc3aed8555d68a24ba1c9951215246c
Nivedita Bhalchandra Wani <niveditawani9@gmail.com>
2026-01-19 09:34:46 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/nivedita/transaction_system/TransactionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nivedita.transaction_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nivedita/transaction_system/TransactionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nivedita.transaction_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
